6c0848a94160a37cf7803e382b8cd5a7588c4faa
Lars Celie <lars.celie@student.hu.nl>
2016-09-12 15:45:37 +0200

    TDD demo init

---
+++ Minor.Dag05.TDDdemo/Minor.Dag05.LarsMath.Test/MathTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Minor.Dag05.LarsMath.Test
+{
+    public class MathTest
+    {
+        [Fact]
+        public void Fact1is1Test()
+        {
+            var target = new Math();
+
+            int result = target.Fact(1);
+
+            Assert.Equal(1, result);
+        }
+
+        [Fact]
+        public void Fact2is2Test()
+        {
+            var target = new Math();
+
+            int result = target.Fact(2);
+
+            Assert.Equal(2, result);
+        }
+
+        [Fact]
+        public void Fact3is6Test()
+        {
+            var target = new Math();
+
+            int result = target.Fact(3);
+
+            Assert.Equal(6, result);
+        }
+
+        [Fact]
+        public void Fact0isExceptionTest()
+        {
+            var target = new Math();
+
+            Exception ex = Assert.Throws<InvalidOperationException>(() => target.Fact(0));
+
+            Assert.Equal(new InvalidOperationException().Message, ex.Message);
+        }
+    }
+}
---
+++ Minor.Dag05.TDDdemo/Minor.Dag05.LarsMath/Math.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Math
+{
+    public int Fact(int n)
+    {
+        if (n < 1)
+        {
+            throw new InvalidOperationException();
+        }
+        return n == 1 ? 1: Fact(n - 1) * n; 
+    }
+}
