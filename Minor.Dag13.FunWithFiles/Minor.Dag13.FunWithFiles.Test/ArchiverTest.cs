d893abdd8cc880f86778a54ee92b14e55c94c53c
Lars Celie <lars.celie@student.hu.nl>
2016-09-26 16:42:03 +0200

    gert

---
+++ Minor.Dag13.FunWithFiles/Minor.Dag13.FunWithFiles.Test/ArchiverTest.cs
@@ -0,0 +1,140 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Minor.Dag13.FunWithFiles.Test
+{
+    [TestClass]
+    public class ArchiverTest
+    {
+
+        private static string DocumentsPath = @"C:\Users\larsc\Documents\";
+        private static string TestFile = @"TestFile.txt";
+
+        [TestMethod]
+        public void DetectNewFileCreation()
+        {
+            // Arrange
+            var target = new Archiver(DocumentsPath);
+
+            if (File.Exists(DocumentsPath + TestFile))
+            {
+                File.Delete(DocumentsPath + TestFile);
+            }
+
+            // Act
+            File.WriteAllText(DocumentsPath + TestFile, "TestDrivenDesignDevelopment");
+
+            Thread.Sleep(100);
+
+            // Assert
+            Assert.AreEqual(1, target.FileCreatedEventCount);
+        }
+
+        [TestMethod]
+        public void DetectChangeEvents()
+        {
+            // Arrange
+            var target = new Archiver(DocumentsPath);
+
+            if (File.Exists(DocumentsPath + TestFile))
+            {
+                File.Delete(DocumentsPath + TestFile);
+            }
+
+            // Act
+            File.WriteAllText(DocumentsPath + TestFile, "TestDrivenDesignDevelopment");
+
+            Thread.Sleep(300);
+
+            using (StreamWriter sw = new StreamWriter(new FileInfo(DocumentsPath + TestFile).OpenWrite()))
+            {
+                sw.WriteLine("Hello");
+            }
+
+            Thread.Sleep(300);
+
+            // Assert
+            Assert.AreEqual(2, target.FileChangedEventCount);
+        }
+
+        [TestMethod]
+        public void DetectNewFileCreationAndChangeContent()
+        {
+            // Arrange
+            var target = new Archiver(DocumentsPath);
+
+            if (File.Exists(DocumentsPath + TestFile))
+            {
+                File.Delete(DocumentsPath + TestFile);
+            }
+
+            // Act
+            File.WriteAllText(DocumentsPath + TestFile, "TestDrivenDesignDevelopment");
+
+            Thread.Sleep(100);
+
+            File.WriteAllText(DocumentsPath + TestFile, "TestDrivenDesignDevelopment2");
+
+            Thread.Sleep(100);
+
+
+            // Assert
+            Assert.AreEqual(1, target.FileCreatedEventCount);
+        }
+
+        [TestMethod]
+        public void DetectDeleteMoveCreationByCopy()
+        {
+            // Arrange
+            var target = new Archiver(DocumentsPath);
+
+            if (File.Exists(DocumentsPath + TestFile))
+            {
+                File.Delete(DocumentsPath + TestFile);
+            }
+
+            // Act
+            File.WriteAllText(DocumentsPath + TestFile, "TestDrivenDesignDevelopment");
+
+            Thread.Sleep(100);
+
+            if (File.Exists(DocumentsPath + @"\test\test.txt"))
+            {
+                File.Delete(DocumentsPath + @"\test\test.txt");
+            }
+
+            File.Move(DocumentsPath + TestFile, DocumentsPath + @"\test\test.txt");
+
+            Thread.Sleep(100);
+
+            // Assert
+            Assert.AreEqual(1, target.FileCreatedEventCount);
+        }
+
+        [TestMethod]
+        public void DetectDeleteMoveCreationByCop2y()
+        {
+            // Arrange
+            var target = new Archiver(DocumentsPath);
+
+            if (File.Exists(DocumentsPath + TestFile))
+            {
+                File.Delete(DocumentsPath + TestFile);
+            }
+
+            // Act
+            File.Create(DocumentsPath + TestFile);
+
+            Thread.Sleep(100);
+
+            // Assert
+            Assert.AreEqual(1, target.FileCreatedEventCount);
+        }
+    }
+}
---
+++ Minor.Dag13.FunWithFiles/Minor.Dag13.FunWithFiles/Archiver.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+
+namespace Minor.Dag13.FunWithFiles
+{
+
+    public class Archiver
+    {
+        private string _documentsPath;
+        private FileSystemWatcher _watcher;
+
+        public Archiver(string documentsPath)
+        {
+            _documentsPath = documentsPath;
+            _watcher = new FileSystemWatcher();
+            _watcher.Path = _documentsPath;
+            _watcher.Filter = "*.txt";
+            _watcher.Created += _watcher_Created;
+            _watcher.Changed += _watcher_Changed;
+            _watcher.EnableRaisingEvents = true;
+        }
+
+        private void _watcher_Changed(object sender, FileSystemEventArgs e)
+        {
+            FileChangedEventCount++;
+        }
+
+        private void _watcher_Created(object sender, FileSystemEventArgs e)
+        {
+            FileCreatedEventCount++;
+        }
+
+        public int FileCreatedEventCount { get; set; }
+        public int FileChangedEventCount { get; set; }
+    }
+
+}
