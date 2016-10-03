af599e64233a84da63cda825c2d7f03ae61c5af0
Lars Celie <lars.celie@student.hu.nl>
2016-10-03 09:51:14 +0200

    forgot ensure created

--- Minor.Dag16.DatabaseTestDriven/Minor.Dag16.DatabaseTestDriven/Minor.Dag16.DatabaseTestDriven/BoogContext.cs
+++ Minor.Dag16.DatabaseTestDriven/Minor.Dag16.DatabaseTestDriven/Minor.Dag16.DatabaseTestDriven/BoogContext.cs
@@ -5,9 +5,10 @@ namespace Minor.Dag16.DatabaseTestDriven
 {
     public class BoogContext : DbContext
     {
-        public BoogContext() {}
-
-        public BoogContext(DbContextOptions<BoogContext> options) : base(options) {}
+        public BoogContext(DbContextOptions<BoogContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
 
         public DbSet<Boog> Bogen { get; set; }
     }
