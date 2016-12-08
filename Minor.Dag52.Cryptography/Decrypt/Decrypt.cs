ac623abcf236394b971cb325b090b07f9431ac9e
Lars Celie <lars.celie@student.hu.nl>
2016-12-08 16:02:29 +0100

    Crypto

---
+++ Minor.Dag52.Cryptography/Decrypt/Decrypt.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decrypt
+{
+    class Decrypt
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Incorrect number of parameters, please provide a file and a password");
+                return;
+            }
+
+            var filePath = args[0];
+            var password = args[1];
+
+            byte[] salt;
+            byte[] IV;
+
+            string decryptedContent = "";
+
+            using (FileStream inputStream = File.OpenRead(filePath))
+            using (StreamReader reader = new StreamReader(inputStream))
+            {
+                salt = Encoding.UTF8.GetBytes(reader.ReadLine());
+                IV = Encoding.UTF8.GetBytes(reader.ReadLine());
+
+                Rfc2898DeriveBytes hasher = new Rfc2898DeriveBytes(password, salt);
+                byte[] key = hasher.GetBytes(16);
+
+                using (Aes algorithm = Aes.Create())
+                using (CryptoStream encryptedStream = new CryptoStream(inputStream,
+                    algorithm.CreateEncryptor(key, IV),
+                    CryptoStreamMode.Read))
+                using (StreamReader read = new StreamReader(encryptedStream))
+                {
+                    read.ReadLine();
+                    read.ReadLine();
+                    decryptedContent = read.ReadToEnd();
+                }
+            }
+
+            using (FileStream inputStream = File.OpenWrite(filePath))
+            using (StreamWriter writer = new StreamWriter(inputStream))
+            {
+                writer.Write(decryptedContent);
+            }
+        }
+    }
+}
---
+++ Minor.Dag52.Cryptography/Minor.Dag52.Cryptography/Encrypt.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minor.Dag52.Cryptography
+{
+    class Encrypt
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Incorrect number of parameters, please provide a file and a password");
+                return;
+            }
+
+            var filePath = args[0];
+            var password = args[1];
+
+            var fileContent = "";
+
+            using (FileStream inputStream = File.OpenRead(filePath))
+            using (StreamReader reader = new StreamReader(inputStream))
+            {
+                fileContent = reader.ReadToEnd();
+            }
+
+            byte[] IV = new byte[16];
+            byte[] salt = new byte[16];
+            RandomNumberGenerator rng = RandomNumberGenerator.Create();
+            rng.GetBytes(IV);
+            rng.GetBytes(salt);
+
+            Rfc2898DeriveBytes hasher = new Rfc2898DeriveBytes(password, salt);
+            byte[] key = hasher.GetBytes(16);
+
+            File.WriteAllText(filePath, $"{salt}\n{IV}\n", Encoding.UTF8);
+
+            using (Aes algorithm = Aes.Create())
+            using (FileStream outputStream = File.OpenWrite(filePath))
+            using (CryptoStream encryptedStream = new CryptoStream(outputStream, algorithm.CreateEncryptor(key, IV), CryptoStreamMode.Write))
+            using (StreamWriter writer = new StreamWriter(encryptedStream))
+            {
+                writer.Write(fileContent);
+            }
+
+            Console.WriteLine($"{filePath} encrypted");
+
+        }
+    }
+}
